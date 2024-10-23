80dbfcd958ce2bde4074ac652ef97094eda3e58d
ReneRajzman <rene.rajzman@student.um.si>
2024-10-24 00:54:51 +0200

    Implemented basic user logic

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-# main.py
 from flask import Flask
 from flask_cors import CORS
-from routes import auth_bp  # Import the authentication routes
+from routes import auth_bp
 
 port = 5551
 
 app = Flask(__name__)
 CORS(app)
 
-# Register the authentication blueprint
-app.register_blueprint(auth_bp, url_prefix='/auth')  # All auth routes will be prefixed with /auth
+
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 if __name__ == '__main__':
     app.run(debug=True, port=port)
--- routes.py
+++ routes.py
@@ -1,17 +1,17 @@
 # routes/auth_routes.py
 from flask import Blueprint, request, jsonify
-from endpoints.user_logic import login_user, register_user  # Import the business logic
+from endpoints.user_logic import login_user, register_user
 
-auth_bp = Blueprint('auth', __name__)  # Define a blueprint for authentication routes
+auth_bp = Blueprint('auth', __name__)
 
-# Route for user login
+
 @auth_bp.route('/login', methods=['POST'])
 def login():
-    user_data = request.json  # Get the JSON data from the request
-    result, status_code = login_user(user_data)  # Call the business logic for login
-    return jsonify(result), status_code  # Return the result and status code
+    user_data = request.json
+    result, status_code = login_user(user_data)
+    return jsonify(result), status_code
 @auth_bp.route('/register', methods=['POST'])
 def register():
-    user_data = request.json  # Get the JSON data from the request (including email)
-    result, status_code = register_user(user_data)  # Call the business logic for registration
-    return jsonify(result), status_code  # Return the result and status code
+    user_data = request.json
+    result, status_code = register_user(user_data)
+    return jsonify(result), status_code

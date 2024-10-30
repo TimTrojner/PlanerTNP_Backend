ce7e058bd3f5a22d554ccfbeae90847fbd4299dc
AlesLenko <ales.lenko@proton.me>
2024-10-30 08:41:12 +0100

    added register blueprint for task_bp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from routes import auth_bp, schedule_bp
+from routes import auth_bp, schedule_bp, task_bp
 
 port = 5551
 
@@ -10,6 +10,7 @@ CORS(app)
 
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(schedule_bp, url_prefix='/schedule')
+app.register_blueprint(task_bp, url_prefix='/task')
 
 if __name__ == '__main__':
     app.run(debug=True, port=port)

cb26c31d0f68fdcc19cda20c71ae31a64e117953
IceFire12 <matickozel@proton.me>
2024-12-04 16:59:33 +0100

    ye

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(schedule_bp, url_prefix='/schedule')
 app.register_blueprint(task_bp, url_prefix='/task')
 
-app_port = int(os.getenv('BACKEND_PORT', 5000))
+app_port = int(os.getenv('BACKEND_PORT', 1234))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=app_port)

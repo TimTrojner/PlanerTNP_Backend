a76fed84a01546d2c2033fc94eeb7875a55adf0a
drupalo <davorin.drozg76@gmail.com>
2024-12-16 21:23:46 +0100

    fix: dns resolver for pymongo added

--- db.py
+++ db.py
@@ -1,15 +1,11 @@
-import os
-
 from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-
 
 class DB:
     def __init__(self):
         # MongoDB connection
         connection_uri = "mongodb+srv://rene:CJW79XEHTXIWD9dp@planertnp.hul1s.mongodb.net/?retryWrites=true&w=majority&appName=planerTNP"
-        #connection_uri = os.getenv('DATABASE_URL')
-        client = MongoClient(connection_uri, server_api=ServerApi('1'))
+        # connection_uri = os.getenv('DATABASE_URL')
+        client = MongoClient(connection_uri)
         self.db = client.dsml
 
         # Ping MongoDB to check connection
--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import os
 
+import dns.resolver
 from flask import Flask
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+
+dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers = ['8.8.8.8']
 
 from routes import auth_bp, schedule_bp, task_bp
 
 app = Flask(__name__)
 CORS(app)
 
+
+
+SWAGGER_URL = "/docs"
+API_URL = "/static/swagger/config.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'PlannerTNP API'
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(schedule_bp, url_prefix='/schedule')
@@ -16,4 +34,4 @@ app.register_blueprint(task_bp, url_prefix='/task')
 app_port = int(os.getenv('BACKEND_PORT', 1234))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=app_port)
+    app.run(debug=True, port=app_port)

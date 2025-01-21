d88357469b0312039d0299a9523a9245b1c4256a
Tim Trojner <tim.trojner@gmail.com>
2025-01-21 19:21:33 +0100

    gecko location fix

--- endpoints/get_wtt_excel.py
+++ endpoints/get_wtt_excel.py
@@ -18,8 +18,7 @@ firefox_profile_path.set_preference("browser.download.dir", download_dir)
 firefox_profile_path.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/vnd.ms-excel")
 firefox_profile_path.set_preference("browser.download.manager.showWhenStarting", False)
 
-# gecko_service = Service("/usr/bin/geckodriver")
-gecko_service = Service("/Users/timtr/geckodriver")
+gecko_service = Service("/usr/bin/geckodriver")
 
 # Configure Firefox options
 firefox_options = Options()
